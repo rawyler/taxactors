f766302ecb06c52085c73f05dd64090c51386761
Thomas Rawyler <rawyler@gmail.com>
2009-10-07 22:29:48 +0200

    added some test(s)

---
+++ src/test/scala/org/rawyler/taxactors/actors/CitizenTest.scala
@@ -0,0 +1,20 @@
+package org.rawyler.taxactors.actors
+
+import _root_.junit.framework.TestSuite
+import _root_.org.junit.Test
+
+class CitizenTest {
+  
+  @Test def testEquality() {
+    val foo = new Citizen("foo", 100000)
+    val foo2 = new Citizen("foo", 100000)
+    assert(foo == foo2)
+  }
+  
+  @Test def testNotEqual() {
+    val foo = new Citizen("foo", 100000)
+    val bar = new Citizen("bar", 100000)
+    assert(foo != bar)
+  }
+  
+}
